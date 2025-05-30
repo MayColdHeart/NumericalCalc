8a2dcf49947020eaa6e63225895bc32d3c1612e1
MayColdHeart <devmayy@gmail.com>
2025-05-29 21:46:52 -0300

    Create Divide vectors operation

--- vectorOperations.ts
+++ vectorOperations.ts
@@ -1,15 +1,80 @@
 
 
-function vectorSum(data_1:number[], data_2:number[]){
+function vectorSum(data_1: number[], data_2: number[]) {
 
-const result = [];
+    const result = [];
 
-for(let i = 0; i < data_1.length; i++){
-    result.push(data_1[i] + data_2[i])
+    for (let i = 0; i < data_1.length; i++) {
+        result.push(data_1[i] + data_2[i])
+    }
+
+    return result;
 }
 
-return result;
+
+function vectorSubtract(data_1: number[], data_2: number[]) {
+
+    const result = []
+
+    for (let i = 0; i < data_1.length; i++) {
+        result.push(data_1[i] - data_2[i])
+    }
+
+    return result;
 }
 
 
-console.log(vectorSum([1, 3, 5, 3, 6, 9],[1, 3, 5, 3, 6, 9]));
+function vectorMultiply(data_1: number[], data_2: number[]) {
+
+    const result = [];
+
+    for (let i = 0; i < data_1.length; i++) {
+        result.push(data_1[i] * data_2[i])
+    }
+
+    return result;
+}
+
+
+
+
+
+
+
+function vectorDivide(data_1: number[], data_2: number[]){
+    
+    const result = []; 
+  
+
+
+    for (let i = 0; i < data_1.length; i++){
+        result.push(data_1[i]/data_2[i]);
+    }
+
+    return result;
+
+}
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(vectorSum([1, 3, 5, 3, 6, 9], [1, 3, 5, 3, 6, 9]));
+console.log(vectorSubtract([3, 89, 5, 3, 8, 9], [1, 3, 55, 3, 89, 9]));
+console.log(vectorMultiply([3, 89, 5, 3, 8, 9], [1, 3, 55, 3, 89, 9]));
+console.log(vectorDivide([3, 89, 5, 3, 8, 9], [1, 3, 55, 3, 89, 9]));
